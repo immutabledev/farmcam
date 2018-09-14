5dac535661efe073712573b75ef7fadd42111a46
immutabledev <immutabledev@users.noreply.github.com>
2018-09-14 04:25:58 +0000

    Moved from POST to SocketIO.

--- farmcam.js
+++ farmcam.js
@@ -2,15 +2,11 @@
 
 // Read configuration
 var fs = require("fs"),
-    spawn = require('child_process').spawn,
-    Foscam = require('foscam-client'),
+    cam = require('node-dahua-api');
     https = require('https'),
-    http = require('http'),
     WebSocket = require('ws'),
     express = require('express'),
     app = express(),
-    cors = require('cors'),
-    exec = require('child_process').exec,
     Forecast = require('forecast');
 
 var conf = JSON.parse(fs.readFileSync("config.json"));
@@ -25,14 +21,32 @@ var URL = 'rtsp://'+USER+':'+PASS+'@'+IP+':'+PORT+'/videoSub';
 var STREAM_SECRET = "farmcam",
         STREAM_PORT = 8081,
         WEBSOCKET_PORT = 8082,
+	SOCKETIO_PORT = 8183,
         RECORD_STREAM = false;
 
-
+var camOptions = {
+	host	: IP,
+	port 	: PORT, 
+	user 	: USER,
+	pass 	: PASS,
+	log 	: false
+};
+var CAMSPEED = '4';
+var CAMDELAYMS = '200';
 console.log("Connecting to Camera: "+URL);
+var camera = new cam.dahua(camOptions);
 
 const key = fs.readFileSync('certs/key.pem');
 const cert = fs.readFileSync('certs/cert.pem');
 const ca = fs.readFileSync('certs/chain.pem');
+
+// SocketIO Server
+const httpsIO = require('https').createServer({
+        key: key,
+        cert: cert,
+        ca: ca
+    }, app);
+const io = require('socket.io').listen(httpsIO);
 
 // Websocket Server
 const httpsServer = https.createServer({
@@ -41,7 +55,6 @@ const httpsServer = https.createServer({
         ca: ca
     }).listen(WEBSOCKET_PORT);
 
-//var socketServer = new WebSocket.Server({server: httpsServer, perMessageDeflate: false});
 var socketServer = new WebSocket.Server( {server: httpsServer} );
 socketServer.connectionCount = 0;
 socketServer.on('connection', function(socket, upgradeReq) {
@@ -105,166 +118,83 @@ var streamServer = http.createServer( function(request, response) {
         }
 }).listen(STREAM_PORT);
 
-// Create the stream from the camera
-/*
-var cmd = '/usr/bin/ffmpeg';
-
-var args = [
-    '-i', URL,
-    '-err_detect', 'ignore_err',
-    '-f', 'mpegts',
-    '-codec:v', 'mpeg1video',
-    '-bf', '0',
-    '-b:v', '180k',
-    '-r', '24',
-    '-codec:a', 'mp2',
-    '-ar', '44100',
-    'http://127.0.0.1:8081/farmcam'
-];
-
-var proc = spawn(cmd, args);
-
-proc.stderr.on('data', function(data) {
-    console.log("[ffmpeg] "+data);
-});
-*/
-
-// Setup camera control
-var camera = new Foscam({
-  username: conf.cam_control_user,
-  password: conf.cam_control_pass,
-  host: IP,
-  port: PORT, // default
-  protocol: 'http', // default
-  rejectUnauthorizedCerts: true // default
-});
-
-https.createServer({
-      key: key,
-      cert: cert,
-      ca: ca 
-    }, app).listen(6999);
-
-var whitelist = ['http://'+conf.domain, 'http://www.'+conf.domain, 'https://'+conf.domain, 'https://www.'+conf.domain];
-var corsOptions = {
-  origin: function(origin, callback){
-    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(null, originIsWhitelisted);
-  }
-};
-
-var forceRestartCmd = conf.restart_cmd;
-var lastRestarted = Date.now();
-
-// Define POST URLs
-app.post('/move/left', cors(corsOptions), function(req, res) {
-	camera.ptzMoveLeft().then(function() {
-		setTimeout(function() {
-                                camera.ptzStopRun()
-                                    .then(function(result) {
-                                        //resolve(result);
-					res.send(req.body);
-                                    })
-                                    .catch(function(result) {
-                                        //reject(result);
-                                    });
-                            }, 200);
-	});
-});
-
-app.post('/move/right', cors(corsOptions), function(req, res) {
-        camera.ptzMoveRight().then(function() {
-                setTimeout(function() {
-                                camera.ptzStopRun()
-                                    .then(function(result) {
-                                        //resolve(result);
-					res.send(req.body);
-                                    })
-                                    .catch(function(result) {
-                                        //reject(result);
-                                    });
-                            }, 200);
-        });
-});
-
-app.post('/move/up', cors(corsOptions), function(req, res) {
-        camera.ptzMoveUp().then(function() {
-                setTimeout(function() {
-                                camera.ptzStopRun()
-                                    .then(function(result) {
-                                        //resolve(result);
-                                        res.send(req.body);
-                                    })
-                                    .catch(function(result) {
-
-                                        //reject(result);
-                                    });
-                            }, 200);
-        });
-});
-
-app.post('/move/down', cors(corsOptions), function(req, res) {
-        camera.ptzMoveDown().then(function() {
-                setTimeout(function() {
-                                camera.ptzStopRun()
-                                    .then(function(result) {
-                                        //resolve(result);
-                                        res.send(req.body);
-                                    })
-                                    .catch(function(result) {
-
-                                        //reject(result);
-                                    });
-                            }, 200);
-        });
-});
-
-app.post('/preset/pond', cors(corsOptions), function(req, res) {
-	camera.ptzGotoPresetPoint('Pond');
-	res.send(req.body);
-});
-
-app.post('/preset/goatdeck', cors(corsOptions), function(req, res) {
-        camera.ptzGotoPresetPoint('GoatDeck');
-	res.send(req.body);
-});
-
-app.post('/preset/bluecottage', cors(corsOptions), function(req, res) {
-        camera.ptzGotoPresetPoint('BlueCottage');
-        res.send(req.body);
-
-});
-
-app.post('/restart_cam', cors(corsOptions), function(req, res) {
-	var rn = Date.now();
-	console.log("Restart Requested: ["+rn+"]["+lastRestarted+"]");
-	if ((rn - lastRestarted)  > 5000) {
-		lastRestarted = rn; 
-		exec(forceRestartCmd, function(error, stdout, stderr) {
-			console.log("Restart commanded!");
-			res.send(req.body);
-		});
-	} else {
-		res.send(req.body);
-	}
-});
-
 // Configure weather
 var forecast = new Forecast({
   service: 'forecast.io',
   key: conf.forecastio_key,
-  units: 'f', // Only the first letter is parsed 
-  cache: true,      // Cache API requests? 
-  ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/ 
+  units: 'f', // Only the first letter is parsed
+  cache: true,      // Cache API requests?
+  ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
     minutes: 5,
-    seconds: 0 
+    seconds: 0
     }
 });
 
-// Make weather data available via GET
-app.get('/forecast', cors(corsOptions), function(req, res) {
-	forecast.get([conf.weather_lat,conf.weather_lon], function(err, weather) {
-		if(err) return res.send(err);
-		res.json(weather);
-	});
+io.on('connection', function(socket){
+  socket.on('moveLeft', function() {
+    console.log('Move left.');
+    moveCam('Left');
+  });
+  socket.on('moveRight', function() {
+    console.log('Move right.');
+    moveCam('Right');
+  });
+  socket.on('moveUp', function() {
+    console.log('Move up.');
+    moveCam('Up');
+  });
+  socket.on('moveDown', function() {
+    console.log('Move down.');
+    moveCam('Down');
+  });
+  socket.on('zoomIn', function() {
+    console.log('Zoom in.');
+    zoomIn();
+  });
+  socket.on('zoomOut', function() {
+    console.log('Zoom out.');
+    zoomOut();
+  });
+  socket.on('gotoPreset', function(preset) {
+    console.log('Goto Preset: '+preset);
+    gotoPreset(preset);
+  });
+  socket.on('getPTZ', function(callback) {
+    console.log('Get PTZ.');
+    callback(camera.ptzStatus());
+  });
+  socket.on('getWeather', function(callback) {
+    console.log('Get weather.');
+    forecast.get([conf.weather_lat,conf.weather_lon], function(err, weather) {
+      callback(json(weather);
+    });
+  });
 });
+
+function moveCam(dir) {
+	camera.ptzMove(dir, 'start', CAMSPEED).then(function() {
+                setTimeout(function() {
+                        camera.ptzMove(dir, 'stop', CAMSPEED)
+                        .then(function(result) {
+				return true;
+                        })
+                        .catch(function(result) {
+				return false;
+                        });
+                }, CAMDELAYMS);
+        });
+}
+
+function zoomIn() {
+	camera.ptzZoom(1.0);
+	return true;
+}
+
+function zoomOut() {
+	camera.ptzZoom(-1.0);
+	return true;
+}
+
+function gotoPreset(preset) {
+	camera.ptzPreset(preset);
+}
